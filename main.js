f916468d418a0827877746be813d907687861e35
lwto <linwathanoo227@gmail.com>
2021-08-23 12:02:47 +0630

    add mobile nav

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 //  Show Menu 
 const toggle = document.getElementById('nav-toggle');
 const navMenu = document.getElementById('nav-menu');
-const close = document.getElementById('nav-close');
-
+const close = document.getElementById('close');
 //Validate that variables exist
 if (toggle) {
     //we add the show-menu class to the div tag with the nav_menu class
     toggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu');
+        toggle.classList.add('hide-toggle');
+        close.classList.add('show-close');
     })
 }
 
 if (close) {
     close.addEventListener('click', () => {
         navMenu.classList.remove('show-menu');
+        toggle.classList.remove('hide-toggle');
+        close.classList.remove('show-close');
     })
 }
 
@@ -21,7 +24,6 @@ if (close) {
 const navLink = document.querySelectorAll('.nav_link');
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu');
     navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
